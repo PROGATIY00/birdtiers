f78b296a8cb9e6031fc13dc23ade30dd46435d96
PROGATIY00 <yorxy.lwk@gmail.com>
2026-05-15 10:44:55 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
+import threading, os
 from index import *
+
+start_mongo_backup_loop()
+threading.Thread(target=lambda: bot.run(TOKEN), daemon=True).start()
+port = int(os.getenv("PORT", "10000"))
+app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
